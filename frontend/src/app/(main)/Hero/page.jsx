71396fd84113ba62a066ea031d98c51be9db3115
sachin2745 <sachin.official648@gmail.com>
2024-07-30 13:28:07 +0530

    hero section is designed

--- frontend/src/app/(main)/Hero/page.jsx
+++ frontend/src/app/(main)/Hero/page.jsx
@@ -1,8 +1,32 @@
+"use client";
 import React from 'react'
+import "./hero.css";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className="text-gray-600 body-font max-w-screen-xl mx-auto -mt-10">
+      <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 ">
+          <h1 className="title-font text-4xl md:text-6xl font-Montserrat  mb-4 font-extrabold text-spaceblack">
+            Journey through knowledge
+
+          </h1>
+          <p className="mb-8 leading-relaxed font-Syne text-lg text-neutral-700">
+            Embark on an enlightening adventure with our curated selection of blog posts. From technology and lifestyle to personal growth and beyond, we bring you content that matters.
+          </p>
+
+        </div>
+        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6  ">
+          <img
+            className="object-cover object-center rounded aspect-square "
+            alt="hero"
+            src="about.png"
+          />
+        </div>
+      </div>
+    </section>
+
+
   )
 }
 
--- frontend/src/app/(main)/Home/page.jsx
+++ frontend/src/app/(main)/Home/page.jsx
@@ -27,71 +27,74 @@ const Home = () => {
   }, []);
 
   return (
-    <section className="text-gray-600 body-font bg-mate_black dark:bg-primary">
+    <>
       <Hero />
-      <div className="text-center font-Jost text-black dark:text-black pt-10">
-        <h1 className="font-bold text-3xl font-Montserrat">My Blog</h1>
-        <h3 className="text-sm font-Montserrat ">Home &rsaquo; My Blog</h3>
-      </div>
+      
+      <section className="text-gray-600 body-font bg-mate_black dark:bg-primary">
+        <div className="text-center font-Jost text-black dark:text-black pt-10">
+          <h1 className="font-bold text-3xl font-Montserrat">My Blog</h1>
+          <h3 className="text-sm font-Montserrat ">Home &rsaquo; My Blog</h3>
+        </div>
 
-      <div className="container px-5 py-24 mx-auto">
-        {loading ? (
-          <div className="flex flex-wrap -m-4">
-            {Array.from({ length: 3 }).map((_, index) => (
-              <div key={index} className="p-4 md:w-1/3">
-                <div className="h-full relative border-2 shadow-xl border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-                  <div className="lg:h-48 md:h-36 w-full bg-gray-300" />
-                  <div className="p-6">
-                    <div className="h-4 bg-gray-300 mb-2" />
-                    <div className="h-4 bg-gray-300 mb-2 w-1/2" />
-                    <div className="h-16 bg-gray-300" />
+        <div className="container px-5 py-24 mx-auto">
+          {loading ? (
+            <div className="flex flex-wrap -m-4">
+              {Array.from({ length: 3 }).map((_, index) => (
+                <div key={index} className="p-4 md:w-1/3">
+                  <div className="h-full relative border-2 shadow-xl border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                    <div className="lg:h-48 md:h-36 w-full bg-gray-300" />
+                    <div className="p-6">
+                      <div className="h-4 bg-gray-300 mb-2" />
+                      <div className="h-4 bg-gray-300 mb-2 w-1/2" />
+                      <div className="h-16 bg-gray-300" />
+                    </div>
                   </div>
                 </div>
-              </div>
-            ))}
-          </div>
-        ) : (
-          <div className="flex flex-wrap -m-4">
-            {postArray.map((post, index) => (
-              <div key={post._id} className="p-4 md:w-1/3 ">
-                <div className="h-full relative border-2 shadow-xl  overflow-hidden border-gray-200 border-opacity-60 rounded-lg ">
-                  <img
-                    className="lg:h-48 md:h-36 w-full object-cover opacity-75  block transition duration-200 ease-out transform hover:opacity-100"
-                    src={`${process.env.NEXT_PUBLIC_API_URL}/${post.image}`}
-                    alt="blog"
-                  />
-                  <span className="absolute font-Oswald outline top-0 z-10 left-0 m-3 rounded-sm bg-transparent tracking-wide px-2 text-center text-sm font-bold text-white">
-                    Blog {index + 1}
-                  </span>
-                  <div className="p-6 font-Josefin_Sans">
-                    <div className='flex justify-between mb-3'>
-                      <h2 className="tracking-widest text-xs title-font font-medium text-gray-600 ">
-                        CATEGORY
+              ))}
+            </div>
+          ) : (
+            <div className="flex flex-wrap -m-4">
+              {postArray.map((post, index) => (
+                <div key={post._id} className="p-4 md:w-1/3 ">
+                  <div className="h-full relative border-2 shadow-xl  overflow-hidden border-gray-200 border-opacity-60 rounded-lg ">
+                    <img
+                      className="lg:h-48 md:h-36 w-full object-cover opacity-75  block transition duration-200 ease-out transform hover:opacity-100"
+                      src={`${process.env.NEXT_PUBLIC_API_URL}/${post.image}`}
+                      alt="blog"
+                    />
+                    <span className="absolute font-Oswald outline top-0 z-10 left-0 m-3 rounded-sm bg-transparent tracking-wide px-2 text-center text-sm font-bold text-white">
+                      Blog {index + 1}
+                    </span>
+                    <div className="p-6 font-Josefin_Sans">
+                      <div className='flex justify-between mb-3'>
+                        <h2 className="tracking-widest text-xs title-font font-medium text-gray-600 ">
+                          CATEGORY
+                        </h2>
+                        <h1 className="title-font text-xs tracking-widest font-medium text-gray-600  uppercase">
+                          {post.category}
+                        </h1>
+                      </div>
+                      <h2 className="leading-relaxed mb-3 font-Montserrat font-bold line-clamp-* capitalize">
+                        {post.title}
                       </h2>
-                      <h1 className="title-font text-xs tracking-widest font-medium text-gray-600  uppercase">
-                        {post.category}
-                      </h1>
-                    </div>
-                    <h2 className="leading-relaxed mb-3 font-Montserrat font-bold line-clamp-* capitalize">
-                      {post.title}
-                    </h2>
-                    <div className="flex items-center flex-wrap  ">
-                      <Link
-                        href={"/Viewblog/" + post._id}
-                        className="text-quaternary  hover:text-white bg-white hover:bg-quaternary px-2 py-2  rounded-md transition ease-in-out  duration-300  transform  inline-flex items-center md:mb-0 lg:mb-0"
-                      >
-                        Learn More <MdKeyboardDoubleArrowRight />
+                      <div className="flex items-center flex-wrap  ">
+                        <Link
+                          href={"/Viewblog/" + post._id}
+                          className="text-quaternary  hover:text-white bg-white hover:bg-quaternary px-2 py-2  rounded-md transition ease-in-out  duration-300  transform  inline-flex items-center md:mb-0 lg:mb-0"
+                        >
+                          Learn More <MdKeyboardDoubleArrowRight />
 
-                      </Link>
+                        </Link>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            ))}
-          </div>
-        )}
-      </div>
-    </section>
+              ))}
+            </div>
+          )}
+        </div>
+      </section>
+    </>
   );
 };
 
